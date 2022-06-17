33284d07b3b24647cc5bdc5302f7f5b064bda1ee
houssein hershy <hhershy@hotmail.com>
2022-06-17 18:00:43 +0300

    Repository Done

---
+++ Services/Catalogs/BeerEShop.Services.Catalogs.Application/Contract/Persistance/IBeerRepository.cs
@@ -0,0 +1,32 @@
+﻿using BeerEShop.Services.Catalogs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerEShop.Services.Catalogs.Application.Contract.Persistance
+{
+    public interface IBeerRepository : IAsyncRepository<Beer>
+    {
+        /// <summary>
+        /// Get List Of Beers By Brewery
+        /// </summary>
+        /// <returns>List of Beers</returns>
+        Task<IEnumerable<Beer>> GetBeers(long BreweryId);
+
+        /// <summary>
+        /// Get Beer By Id
+        /// </summary>
+        /// <param name="beerId"> Beer Id</param>
+        /// <returns>Product</returns>
+        Task<Beer> GetBeerById(long beerId);
+
+        /// <summary>
+        /// Delete Beer By Id
+        /// </summary>
+        /// <param name="beerId"> Beer Id</param>
+        /// <returns>Product</returns>
+         Task<bool> DeleteBeerById(long beerId);
+    }
+}
---
+++ Services/Catalogs/BeerEShop.Services.Catalogs.Infrastracture/Repositories/BeerRepository.cs
@@ -0,0 +1,47 @@
+﻿using BeerEShop.Services.Catalogs.Application.Contract.Persistance;
+using BeerEShop.Services.Catalogs.Domain.Entities;
+using BeerEShop.Services.Catalogs.Domain.ValueObjects;
+using BeerEShop.Services.Catalogs.Infrastracture.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerEShop.Services.Catalogs.Infrastracture.Repositories
+{
+    public class BeerRepository : RepositoryBase<Beer> , IBeerRepository
+    {
+        public BeerRepository(BeerCatalogContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<bool> DeleteBeerById(long beerId)
+        {
+            var Beer = await GetBeerById(beerId);
+            Beer.ChangeStatus(BeerStatus.Unavailable);
+            await _dbContext.SaveChangesAsync();
+            return true;
+
+        }
+
+        public async Task<Beer> GetBeerById(long beerId)
+        {
+            var Beer = await _dbContext.Beers.Where(p => p.BeerId == beerId)
+                              .Include("Brewery")
+                               .SingleOrDefaultAsync();
+            return Beer;
+
+        }
+
+        public async Task<IEnumerable<Beer>> GetBeers(long BreweryId)
+        {
+            var Beers = await _dbContext.Beers.Where(p => p.BreweryId == BreweryId)
+                              .Include("Brewery")
+                               .ToListAsync();
+            return Beers;
+        }
+    }
+}
