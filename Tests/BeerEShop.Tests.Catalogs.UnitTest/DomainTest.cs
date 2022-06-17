cb7a5453627f86e7166cddbfda821c2b94fb5c69
houssein hershy <hhershy@hotmail.com>
2022-06-17 17:35:43 +0300

    Test Beer Brewery

---
+++ Tests/BeerEShop.Tests.Catalogs.UnitTest/DomainTest.cs
@@ -0,0 +1,56 @@
+using BeerEShop.Services.Catalogs.Domain.Entities;
+using BeerEShop.Services.Catalogs.Domain.ValueObjects;
+using BeerEShop.Tests.Catalogs.UnitTest.Models;
+using System;
+using Xunit;
+
+namespace BeerEShop.Tests.Catalogs.UnitTest
+{
+    public class DomainTest
+    {
+        [Fact]
+        public void AbeerIsNecessarilyLinkedToABrewer()
+        {
+           
+                TBeer BeerModel = new TBeer();
+                BeerModel.Name = "Leffe Blonde";
+                BeerModel.BeerStatus = BeerStatus.Available;
+                BeerModel.Volume = 5.2m;
+                BeerModel.AlcoholContent = 6.6m;
+                BeerModel.Price = 3.5m;
+                BeerModel.SellingPrice = 100;
+                BeerModel.BreweryId = null;
+           
+                Assert.Throws<InvalidOperationException>(() => Beer.Create(BeerModel.Name, BeerModel.BeerStatus, BeerModel.Volume, BeerModel.AlcoholContent, BeerModel.Price, BeerModel.SellingPrice, BeerModel.BreweryId.Value));
+           
+        }
+
+        [Fact]
+        public void AbeerIsLinked()
+        {
+
+            TBeer BeerModel = new TBeer();
+            BeerModel.Name = "Leffe Blonde";
+            BeerModel.BeerStatus = BeerStatus.Available;
+            BeerModel.Volume = 5.2m;
+            BeerModel.AlcoholContent = 6.6m;
+            BeerModel.Price = 3.5m;
+            BeerModel.SellingPrice = 100;
+            BeerModel.BreweryId = 1;
+
+            Beer Result = Beer.Create(BeerModel.Name, BeerModel.BeerStatus, BeerModel.Volume, BeerModel.AlcoholContent, BeerModel.Price, BeerModel.SellingPrice, BeerModel.BreweryId.Value);
+            Assert.Equal(1, Result.BreweryId);
+
+        }
+
+        [Fact]
+        public void ABrewerBrewsOneToSeveralBeers()
+        {
+
+           var FirstBeer =  Beer.Create("Leffe Blonde", BeerStatus.Available, 5.2m, 100, 115, 130, 1);
+            var SecondBeer = Beer.Create("Imperial Enchanted Rain Rocket", BeerStatus.Available, 6.6m, 70, 80, 130, 1);
+            Assert.Equal(FirstBeer.BreweryId, SecondBeer.BreweryId);
+
+        }
+    }
+}
---
+++ Tests/BeerEShop.Tests.Catalogs.UnitTest/Models/TBeer.cs
@@ -0,0 +1,20 @@
+﻿using BeerEShop.Services.Catalogs.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerEShop.Tests.Catalogs.UnitTest.Models
+{
+    public class TBeer
+    {
+        public string Name { get; set; }
+        public decimal AlcoholContent { get; set; }
+        public decimal Price { get; set; }
+        public decimal SellingPrice { get; set; }
+        public decimal Volume { get; set; }
+        public BeerStatus BeerStatus { get; set; }
+        public long? BreweryId { get; set; }
+    }
+}
